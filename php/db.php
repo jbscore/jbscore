3c4cb9b20c1094171ce56fba1902e604df4b4f3b
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-08-08 10:52:26 +0700

    Initial message

--- php/db.php
+++ php/db.php
@@ -1,3 +1,6 @@
 <?php
-$c = new mysqli("localhost", "user1", "HelloWorld123");
-$c->select_db("jbscore");
+/*$c = new mysqli("localhost", "user1", "HelloWorld123");
+$c->select_db("jbscore");*/
+
+$c = new mysqli("localhost", "u4785556_user1", "HelloWorld123");
+$c->select_db("u4785556_jbscore");
